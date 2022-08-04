ce0206016dbb8c1b16263e75b1a67e0b7820739c
星期三 <417666017@qq.com>
2022-08-04 11:17:25 +0800

    node安装的npm

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURL: 'mongodb://admin:a12345@localhost:27017',
+  secretorKey: 'secret',
+};
--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-//
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const passport = require('passport');
+// 引入数据库
+
+const db = require('./config/keys').mongoURL;
+// 引入users.js
+const users = require('./routes/api/users.js');
+const profiles = require('./routes/api/profiles');
+//引入body-Parser
+const bodyParser = require('body-parser');
+// 使用body-Paeser中间件
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//使用routes
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
+const port = process.env.PORT || 5000;
+mongoose
+  .connect(db)
+  .then(() => console.log('连接成功'))
+  .catch((err) => console.log(err));
+// passport初始化
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
